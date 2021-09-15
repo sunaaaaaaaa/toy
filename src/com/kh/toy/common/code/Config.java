8a5659986ce2f3d267276d7493472d4066a76c21
sunaaaaaaaa <tjsdk7075@naver.com>
2021-09-15 19:29:41 +0900

    [최선아] Egit테스트

--- src/com/kh/toy/common/code/Config.java
+++ src/com/kh/toy/common/code/Config.java
@@ -4,9 +4,9 @@ public enum Config {
 	
 	//DOMAIN("나중에 배포할 도메인"),
 	DOMAIN("http://local:7070"),
-	SMTP_AUTHENTICATION_ID("tjsdk7075@gmail.com"),
-	SMTP_AUTHENTICATION_PASSWORD("Insert1226!"),
-	COMPANY_EMAIL("tjsdk7075@gmail.com"),
+	SMTP_AUTHENTICATION_ID("tjsdk705@gmail.com"),
+	SMTP_AUTHENTICATION_PASSWORD("1234"),
+	COMPANY_EMAIL("tjsdk705@gmail.com"),
 	//UPLOAD_PATH("C:\\CODE\\upload\\"), //운영서버용
 	UPLOAD_PATH("C:\\CODE\\upload\\"); //개발서버용
 	
---
+++ src/text/http/GitTest.java
@@ -0,0 +1,9 @@
+package text.http;
+
+public class GitTest {
+
+	private static final String DESC ="local 깃 테스트용 클래스입니당";
+	
+	
+	
+}
