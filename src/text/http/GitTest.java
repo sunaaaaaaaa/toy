cde351196fc1ad7f47b48a0c58a666c1b516b4e4
sunaaaaaaaa <89139744+sunaaaaaaaa@users.noreply.github.com>
2021-09-15 20:07:28 +0900

    conflict test

--- src/text/http/GitTest.java
+++ src/text/http/GitTest.java
@@ -2,8 +2,8 @@ package text.http;
 
 public class GitTest {
 
-	private static final String DESC ="local 깃 테스트용 클래스입니당";
+	private static final String DESC ="[GITHUB]local 깃 테스트용 클래스입니당";
 	
-	private static final String DEV = "dev브랜치 테스트 ㅅㅣ작";
-	private static final String DEV_FIN = "dev 브랜치테스트종료";
+	private static final String DEV = "[GITHUB]dev브랜치 테스트 ㅅㅣ작";
+	private static final String DEV_FIN = "[GITHUB]dev 브랜치테스트종료";
 }
